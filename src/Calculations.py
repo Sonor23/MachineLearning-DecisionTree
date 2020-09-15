abf7f0fa7c3bee0a5477b2e4b77d14841403f9c4
Sondre <Sondre.Omland@student.uib.no>
2020-09-15 10:55:01 +0200

    Fikset litt og la til noen ekstra verdier i Nodekonstruktør

--- src/Calculations.py
+++ src/Calculations.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 
 
+
 # Calculating Entropy => H(X)
 def calcEntropy(col):
     entropy = - sum([p * math.log(p) / math.log(2.0) for p in col])
@@ -16,7 +17,10 @@ def calcJointEntropy(col1, col2):
 def calcCondEntropy(col1, col2):
     return calcJointEntropy(col1, col2) - calcEntropy(col2)
 
-
 # Calculating information gain I(Y;X) = H(X) - H(Y|X)
 def informationGain(col1, col2):
     return calcEntropy(col1) - calcCondEntropy(col1, col2)
+
+# Calculating gini
+def gini(val):
+    return val ** 2
--- src/Node.py
+++ src/Node.py
@@ -1,13 +1,16 @@
-
+from .Calculations import *
 
 
 class Node:
-    def __init__(self, X, y, parent, impurity_measure):
+    def __init__(self, X, y, parent, treshold, impurity_measure):
         self.X = X
         self.y = y
         self.impurity_measure = impurity_measure
         self.children = []
         self.parent = parent
+        self.treshold = treshold  #Value for split
+        self.left = None
+        self.right = None
 
 
     # Adding children to tree
@@ -23,6 +26,18 @@ class Node:
             return self
 
         else:
+            pass
+
+    def chooseImpurity(self, col):
+        if self.impurity_measure == 'entropy':
+            return calcEntropy(col)
+        elif self.impurity_measure == 'gini':
+            return gini(col)
+
+        else:
+            print("Impurity_measure -> Not walid")
+
+
 
 
 
