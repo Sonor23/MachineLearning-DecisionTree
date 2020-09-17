ca59fb09a0ff84cb0b73473966837f7847717e29
ruy010 <Silje.moen@student.uib.no>
2020-09-17 19:21:58 +0200

    Started on learn method

--- src/DTree.py
+++ src/DTree.py
@@ -19,6 +19,13 @@ def main():
     y = Y_train
     impurity_measure = 'entropy'
 
+    count_distribution_of_labels = X.label.value_counts()
+    print(" dist")
+    print(count_distribution_of_labels)
+    print("length of dist")
+    print(len(count_distribution_of_labels))
+    print("EEN")
+
     find_best_column_to_split_entropy(X)
 
     # tree = learn(X, y, impurity_measure)
@@ -100,8 +107,9 @@ def find_best_column_to_split_entropy(data_frame):
         column_names.remove("label")
 
     column_entropies = {}
+    column_means = {}
 
-    for column_name in attributes:
+    for column_name in column_names:
         column = data_frame.get(column_name)
         column_mean = column.mean(axis=0)  # threshold for column
 
@@ -117,56 +125,85 @@ def find_best_column_to_split_entropy(data_frame):
         entropy_for_column = probability_below * entropy_below + probability_above * entropy_above
 
         column_entropies[column_name] = entropy_for_column
+        column_means[column_name] = column_mean
 
     column_name_min_value = min(column_entropies, key=column_entropies.get)
 
-    return column_name_min_value
+    return column_name_min_value, column_means[column_name_min_value]
 
 
-# choose which column to split by finding entropies for all columns
-def chooseSplitColumnWithEntropy(X, y, thresholdList):
-    # number of elements above and under thresholds in each column of X
-    smallerX, biggerX = countSmallerAndBigger(X, thresholdList)
-
-    # find conditional entropies for each column
-    entropiesLabelGivenColumn = {}
-
-    # (label|value)
-    for col in range(X.shape[1]):
-        # probability of values above threshold
-        probXBig = biggerX[col] / (biggerX[col] + smallerX[col])
-        # probability of values smaller than threshold
-        probXSmall = smallerX[col] / (biggerX[col] + smallerX[col])
-
-        # split into set of labels given value above and under threshold
-        yGivenValueBig = []
-        yGivenValueSmall = []
-
-        for row in range(X.shape[0]):
-            if (X[row][col] >= thresholdList[col]):
-                yGivenValueBig.append(y[row])
-            else:
-                yGivenValueSmall.append(y[row])
-
-        entropyYgivenValueBig = entropy(yGivenValueBig)
-        entropyYgivenValueSmall = entropy(yGivenValueSmall)
-
-        entropyYgivenCol = probXSmall * entropyYgivenValueSmall + probXBig * entropyYgivenValueBig
-        entropiesLabelGivenColumn[col] = entropyYgivenCol
-
-    # find min of the entropies we get by splitting the different columns
-    minValue = min(entropiesLabelGivenColumn.values())
-    indexOfMin = list(entropiesLabelGivenColumn.values()).index(minValue)
-    # return column with lowest entropy, which means highest information gain
-    return indexOfMin
+# check if all labels are the same
+def check_all_same_label(data_frame):
+    count_distribution_of_labels = data_frame.label.value_counts()
+    if len(count_distribution_of_labels) == 1:
+        return True
+    return False
 
 
-def learn(X, y, impurity_measure='entropy'):
-    return
+def check_all_same_values(data_frame):
+    column_names = list(data_frame.columns.values)
+
+    if "label" in column_names:
+        column_names.remove("label")
+
+    df_without_duplicates = data_frame.drop_duplicates()
+    if len(df_without_duplicates.index) == 1:
+        return True
+    return False
+
+
+def learn(X, y, impurity_measure="entropy"):
+    # all data points have the same label
+    if (check_all_same_label(X)):
+        tree = Node("Leaf", y[0], None)
+        tree.leaf = True
+        return tree
+
+    # all data points have identical feature values
+    elif check_all_same_values(X):
+        most_common_label = X.label.mode() # most common label
+
+        # return leaf with most common label
+        tree = Node("Leaf", most_common_label, None)
+        tree.leaf = True
+        return tree
+
+    else:
+
+        if (impurity_measure == "gini"):
+            print("pls, add gini support to learn method")
+            column_name, column_threshold = "Bullshit"
+            # column = chooseSplitColumnWithGini(X, y, thresholdList)  # gini index
+        else:
+            column_name, column_threshold = find_best_column_to_split_entropy(X)
+
+        # make node for the column we are splitting on
+        tree = Node(column_name, None, column_threshold)
+
+        #TODO
+        # split input matrix and label vector into new matrices and label vectors and do ID3 on them
+        X0, y0, X1, y1 = split(X, y, column, thresholdList[column])
+        tree.left = ID3(X0, y0, columnNames, impurity_measure)
+        tree.right = ID3(X1, y1, columnNames, impurity_measure)
+        tree.children = [tree.left, tree.right]
+
+        return tree
 
 
 def predict(x, tree):
     pass
+
+
+class Node:
+    def __init__(self, column_name, label, threshold):
+        self.left = self
+        self.right = self
+        self.columnName = column_name
+        self.label = label
+        self.threshold = threshold
+        self.leaf = False
+        self.children = None
+        self.majCount = 0
 
 
 main()
