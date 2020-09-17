ac47919c9ce85f2477940ad570de78f5ee33fdc0
ruy010 <Silje.moen@student.uib.no>
2020-09-17 20:54:18 +0200

    Added predict

--- src/DTree.py
+++ src/DTree.py
@@ -12,7 +12,7 @@ class Node:
     def __init__(self, column_name, label, threshold):
         self.left = self
         self.right = self
-        self.columnName = column_name
+        self.column_name = column_name
         self.label = label
         self.threshold = threshold
         self.leaf = False
@@ -29,17 +29,42 @@ def main():
     # List of labels corresponding to X
     y = Y_train
     impurity_measure = 'entropy'
-    print("gini")
-    column_name1, column_threshold1 = find_best_column_to_split_gini(X)
-    print(column_name1)
-    print(column_threshold1)
 
-    print("entro")
-    column_name2, column_threshold2 = find_best_column_to_split_entropy(X)
-    print(column_name2)
-    print(column_threshold2)
+    tree = learn(X, y, impurity_measure)
 
-    # tree = learn(X, y, impurity_measure)
+    print("____LABEL____Should be 1")
+    real1 = [-3.1158, -8.6289, 10.4403, 0.97153]
+    real2 = [-2.0891, -0.48422, 1.704, 1.7435]
+    real3 = [-1.6936, 2.7852, -2.1835, -1.9276]
+    real4 = [-1.2846, 3.2715, -1.7671, -3.2608]
+    real5 = [-0.092194, 0.39315, -0.32846, -0.13794]
+    real6 = [-1.0292, -6.3879, 5.5255, 0.79955]
+    real7 = [-2.2083, -9.1069, 8.9991, -0.28406]
+    real8 = [-1.0744, -6.3113, 5.355, 0.80472]
+    real = [real1, real2, real3, real4, real5, real6, real7, real8]
+    for row in real:
+        predicted_label = predict(row, tree)
+        print(predicted_label)
+
+    print("____LABEL____Should be 0")
+    a = [3.82, 10.9279, -4.0112, -5.0284]
+    b = [-2.7419, 11.4038, 2.5394, -5.5793]
+    c = [3.3669, -5.1856, 3.6935, -1.1427]
+    d = [4.5597, -2.4211, 2.6413, 1.6168]
+    e =[5.1129, -0.49871, 0.62863, 1.1189]
+    abc = [a,b,c,d,e]
+    for row in abc:
+        predicted_label = predict(row, tree)
+        print(predicted_label)
+
+    possible_row_false_1 = [3.62160, 8.6661, -2.8073, -0.44699]
+    possible_row_real_1 = [-0.092194, 0.39315, -0.32846, -0.13794]
+
+    predicted_label_1 = predict(possible_row_false_1, tree)
+    predicted_label_2 = predict(possible_row_real_1, tree)
+    print("____LABEL other____")
+    print(predicted_label_1)
+    print(predicted_label_2)
 
     # print(print_tree(tree))
 
@@ -143,7 +168,7 @@ def find_best_column_to_split_gini(data_frame):
 
         column_ginies[column_name] = gini_for_column
         column_means[column_name] = column_mean
-        print(column_name + " " + str(gini_for_column))
+        #print(column_name + " " + str(gini_for_column))
 
     column_name_min_value = min(column_ginies, key=column_ginies.get)
 
@@ -177,7 +202,7 @@ def find_best_column_to_split_entropy(data_frame):
         column_entropies[column_name] = entropy_for_column
         column_means[column_name] = column_mean
 
-        print(column_name + " " + str(entropy_for_column))
+        #print(column_name + " " + str(entropy_for_column))
 
     column_name_min_value = min(column_entropies, key=column_entropies.get)
 
@@ -215,13 +240,13 @@ def split(data_frame, column_name, threshold):
 def learn(X, y, impurity_measure="entropy"):
     # all data points have the same label
     if check_all_same_label(X):
-        tree = Node("Leaf", y[0], None)
+        tree = Node("Leaf", X.label.mode().get(0), None)
         tree.leaf = True
         return tree
 
     # all data points have identical feature values
     elif check_all_same_values(X):
-        most_common_label = X.label.mode()  # most common label
+        most_common_label = X.label.mode().get(0)  # most common label
 
         # return leaf with most common label
         tree = Node("Leaf", most_common_label, None)
@@ -248,13 +273,19 @@ def learn(X, y, impurity_measure="entropy"):
         return tree
 
 
-def predict(x, tree):
-    pass
+def predict(x, tree: Node):
+    while not tree.leaf:
+        column_index = all_attributes.index(tree.column_name)
+        if x[column_index] < tree.threshold:
+            tree = tree.left
+        else:
+            tree = tree.right
+    return tree.label
 
 
 # return a string of the tree that can be printed
 def print_tree(tree: Node, level=0, condition=None):
-    ret = "\t" * level + str(tree.columnName) + " (" + str(tree.threshold) + ")" + "label: " + str(tree.label) + " "
+    ret = "\t" * level + str(tree.column_name) + " (" + str(tree.threshold) + ")" + "label: " + str(tree.label) + " "
     if condition:
         ret += str(condition)
     ret += "\n"
