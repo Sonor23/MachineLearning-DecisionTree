6d23f13e14fd88f0eb91c8d02111c4964618caf4
ruy010 <Silje.moen@student.uib.no>
2020-09-18 14:14:25 +0200

    Started with pruning, not really working lacks method to count majority label of sub tree

--- src/DTree.py
+++ src/DTree.py
@@ -7,12 +7,60 @@ all_attributes = ["variance", "skewness", "curtosis ", "entropy"]
 
 class Node:
     def __init__(self, column_name, label, threshold):
-        self.left = self
-        self.right = self
+        self.left = None
+        self.right = None
         self.column_name = column_name
         self.label = label
         self.threshold = threshold
         self.leaf = False
+        self.children = None
+
+    def prune(self, prune_data, y, tree):
+        if not self.leaf:
+            if self.left is not None:
+                self.left.prune(prune_data, y, tree)
+            if self.right is not None:
+                self.right.prune(prune_data, y, tree)
+        else:
+            return
+
+        accuracy_before_pruning = accuracy(prune_data, tree)
+        print(accuracy_before_pruning)
+        right_branch = self.right
+        left_branch = self.left
+        print(self.majority_label(self))
+        self.label = self.majority_label(self)
+        self.right = None
+        self.left = None
+        self.leaf = True
+
+        accuracy_after_pruning = accuracy(prune_data, tree)
+        print(accuracy_after_pruning)
+
+        if accuracy_after_pruning < accuracy_before_pruning:
+            self.label = None
+            self.right = right_branch
+            self.left = left_branch
+            self.left = False
+
+    def majority_label(self, tree):
+        label_0 = 0
+        label_1 = 0
+        if self.leaf:
+            if self.label == 0:
+                label_0 = label_0 + 1
+            elif self.label == 1:
+                label_1 = label_1 + 1
+            return
+
+        if self.left is not None:
+            self.majority_label(self.left)
+        if self.right is not None:
+            self.majority_label(self.right)
+
+        if label_0 > label_1:
+            return 0
+        return 1
 
 
 def main():
@@ -26,9 +74,19 @@ def main():
     y = Y_train
     impurity_measure = 'gini'
 
+    X_prune = X.sample(frac=0.15, random_state=0)
+    X = X.drop(X_prune.index)
+
     tree = learn(X, y, impurity_measure)
-    acc = accuracy(X_test, tree)
+
+    acc = accuracy(X_test.copy(), tree)
     print(acc)
+
+    tree.prune(X_prune, y, tree)
+
+    new_acc = accuracy(X_test.copy(), tree)
+
+    print(new_acc)
 
 
 def load_data():
@@ -169,8 +227,14 @@ def split(data_frame, column_name, threshold):
     return above, below
 
 
-def learn(X, y, impurity_measure="entropy"):
+def learn_with_pruning(X, y, impurity_measure="entropy", prune=False):
     # all data points have the same label
+
+    if prune:
+        X_train = X.sample(frac=0.15, random_state=0)
+        X = X.drop(X_train.index)
+        prune = False
+
     if check_all_same_label(X):
         tree = Node("Leaf", X.label.mode().get(0), None)
         tree.leaf = True
@@ -196,13 +260,44 @@ def learn(X, y, impurity_measure="entropy"):
 
         tree.left = learn(above, y, impurity_measure)
         tree.right = learn(below, y, impurity_measure)
+        tree.children = [tree.left, tree.right]
+        return tree
+
+
+def learn(X, y, impurity_measure="entropy"):
+    # all data points have the same label
+    if check_all_same_label(X):
+        tree = Node("Leaf", X.label.mode().get(0), None)
+        tree.leaf = True
+        return tree
+
+    # all data points have identical feature values
+    elif check_all_same_values(X):
+        most_common_label = X.label.mode().get(0)  # most common label
+        # return leaf with most common label
+        tree = Node("Leaf", most_common_label, None)
+        tree.leaf = True
+        return tree
+
+    else:
+        if impurity_measure == "gini":
+            column_name, column_threshold = find_best_column_to_split_gini(X)
+        else:
+            column_name, column_threshold = find_best_column_to_split_entropy(X)
+
+        tree = Node(column_name, None, column_threshold)
+
+        above, below = split(X, column_name, column_threshold)
+
+        tree.left = learn(below, y, impurity_measure)
+        tree.right = learn(above, y, impurity_measure)
         return tree
 
 
 def predict(x, tree):
     while not tree.leaf:
         column_index = all_attributes.index(tree.column_name)
-        if x[column_index] > tree.threshold:
+        if x[column_index] <= tree.threshold:
             tree = tree.left
         else:
             tree = tree.right
@@ -210,10 +305,11 @@ def predict(x, tree):
 
 
 def accuracy(test_data, tree):
+    test_copy = test_data.copy()
     correct = 0
-    length = len(test_data)
-    test_label = test_data.pop("label")
-    for index, row in test_data.iterrows():
+    length = len(test_copy)
+    test_label = test_copy.pop("label")
+    for index, row in test_copy.iterrows():
         prediction = predict(list(row), tree)
         if prediction == test_label[index]:
             correct = correct + 1
