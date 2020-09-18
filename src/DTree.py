d30a175f39dbb63e6dc49196afac8f3ca60d2f0b
ruy010 <Silje.moen@student.uib.no>
2020-09-18 18:13:25 +0200

    Learn method supports pruning

--- src/DTree.py
+++ src/DTree.py
@@ -23,15 +23,15 @@ class Node:
         else:
             return
 
-        accuracy_before_pruning = accuracy(prune_data, tree)
+        accuracy_before_pruning = accuracy(prune_data, self)
         right_branch = self.right
         left_branch = self.left
-        self.label = majority_count(tree)
+        self.label = majority_count(self)
         self.right = None
         self.left = None
         self.is_leaf = True
 
-        accuracy_after_pruning = accuracy(prune_data, tree)
+        accuracy_after_pruning = accuracy(prune_data, self)
 
         if accuracy_after_pruning < accuracy_before_pruning:
             self.label = None
@@ -40,15 +40,13 @@ class Node:
             self.is_leaf = False
 
 
-def majority_count(tree):
-    num_of_leaves = count_leaves(tree)
-    sum_labels_in_leaves = sum_of_labels(tree)
-    if sum_labels_in_leaves < (num_of_leaves / 2):
+def majority_count(tree):  # majority label in T
+    if sum_of_labels(tree) < (count_leaves(tree) / 2):
         return 0
     return 1
 
 
-def count_leaves(tree):
+def count_leaves(tree):  # counts leaves in T
     if tree is None:
         return 0
     if tree.left is None and tree.right is None:  # basically leaf
@@ -66,70 +64,27 @@ def sum_of_labels(tree):
         return sum_of_labels(tree.left) + sum_of_labels(tree.right)
 
 
-def main():
-    seed = 1
-    data = load_data()
-    X_train, X_test, Y_train, Y_test = create_training_and_test_df(data, seed)
-
-    # Matrix - Part of Data without labels
-    X = X_train
-    # List of labels corresponding to X
-    y = Y_train
-    impurity_measure = 'gini'
-
-    X_prune = X.sample(frac=0.15, random_state=0)
-    X = X.drop(X_prune.index)
-
-    tree_gini = learn(X.copy(), y, "gini")
-    tree_entropy = learn(X.copy(), y)
-
-    acc_gini = accuracy(X_test.copy(), tree_gini)
-    acc_entropy = accuracy(X_test.copy(), tree_entropy)
-
-    print("Accuracy before pruning")
-    print("--Gini")
-    print(acc_gini)
-    print("--Entropy")
-    print(acc_entropy)
-
-    tree_gini.prune(X_prune, tree_gini)
-    tree_entropy.prune(X_prune, tree_entropy)
-
-    new_acc_gini = accuracy(X_test.copy(), tree_gini)
-    new_acc_entropy = accuracy(X_test.copy(), tree_entropy)
-
-    print("Accuracy after pruning")
-    print("--Gini")
-    print(new_acc_gini)
-    print("--Entropy")
-    print(new_acc_entropy)
-
-
 def load_data():
     print("Load Data")
     data = pd.read_csv('data_banknote_authentication.txt', header=None)
     data.columns = all_column_names
 
-    # Printing the dataswet shape
+    # Print shape of loaded data
     print("Dataset Length: ", len(data))
     print("Dataset Shape: ", data.shape)
 
-    # Printing the dataset obseravtions
+    # Print head of loaded data
     print("Head of Dataset: ", data.head())
     return data
 
 
-def create_training_and_test_df(data, seed, trainPrecentage=0.7):
+def create_training_and_test_df(data, train_percentage=0.7):
     data_copy = data.copy()
-    X_train = data_copy.sample(frac=trainPrecentage, random_state=0)
-    X_test = data_copy.drop(X_train.index)
-    Y_train = X_train.get('label')
-    Y_test = X_test.get('label')
-
-    # TODO How to pop away the labels
-    # Y_train = X_train.pop('label')
-    # Y_test = X_test.pop('label')
-    return X_train, X_test, Y_train, Y_test
+    x_train = data_copy.sample(frac=train_percentage, random_state=0)
+    x_test = data_copy.drop(x_train.index)
+    y_train = x_train.get('label')
+    y_test = x_test.get('label')
+    return x_train, x_test, y_train, y_test
 
 
 def calc_entropy(data_frame):
@@ -143,7 +98,6 @@ def calc_entropy(data_frame):
     return -entropy
 
 
-# calculate gini index
 def calc_gini(data_frame):
     len_y = len(data_frame)
     count_distribution_of_labels = data_frame.label.value_counts()
@@ -215,7 +169,7 @@ def split(data_frame, column_name, threshold):
     return above, below
 
 
-def learn(X, y, impurity_measure="entropy"):
+def make_tree(X, y, impurity_measure="entropy"):
     # all data points have the same label
     if check_all_same_label(X):
         tree = Node("Leaf", X.label.mode().get(0), None)
@@ -238,9 +192,21 @@ def learn(X, y, impurity_measure="entropy"):
 
         above, below = split(X, column_name, column_threshold)
 
-        tree.left = learn(below, y, impurity_measure)
-        tree.right = learn(above, y, impurity_measure)
+        tree.left = make_tree(below, y, impurity_measure)
+        tree.right = make_tree(above, y, impurity_measure)
         return tree
+
+
+def learn(X, y, impurity_measure="entropy", prune=False):
+    if prune:
+        x_pruning_data = X.sample(frac=0.15, random_state=0)
+        X = X.drop(x_pruning_data.index)
+    tree = make_tree(X, y, impurity_measure)
+
+    if prune:
+        tree.prune(x_pruning_data, tree)
+
+    return tree
 
 
 def predict(x, tree: Node):
@@ -265,27 +231,38 @@ def accuracy(test_data, tree: Node):
     return correct / length
 
 
+def main():
+    data = load_data()
+    X_train, X_test, Y_train, Y_test = create_training_and_test_df(data)
+
+    # Matrix - Part of Data without labels
+    X = X_train
+    # List of labels corresponding to X
+    y = Y_train
+
+    tree_gini = learn(X.copy(), y, "gini")
+    tree_entropy = learn(X.copy(), y)
+
+    acc_gini = accuracy(X_test.copy(), tree_gini)
+    acc_entropy = accuracy(X_test.copy(), tree_entropy)
+
+    print("Accuracy before pruning")
+    print("--Gini")
+    print(acc_gini)
+    print("--Entropy")
+    print(acc_entropy)
+
+    tree_gini_prune = tree_gini = learn(X.copy(), y, impurity_measure="gini", prune=True)
+    tree_entropy_prune = tree_entropy = learn(X.copy(), y, prune=True)
+
+    new_acc_gini = accuracy(X_test.copy(), tree_gini_prune)
+    new_acc_entropy = accuracy(X_test.copy(), tree_entropy_prune)
+
+    print("Accuracy after pruning")
+    print("--Gini")
+    print(new_acc_gini)
+    print("--Entropy")
+    print(new_acc_entropy)
+
+
 main()
-
-
-def div(X, y, column_name):
-    # TODO another way to get a column
-    #  my_column = X[[columnName]]
-
-    column = X.get(column_name)
-    column_mean = column.mean(axis=0)  # threshold for column
-
-    above_real = X.loc[(X[column_name] > column_mean) & (X['label'] == 1)]
-    above_fake = X.loc[(X[column_name] > column_mean) & (X['label'] == 0)]
-    below_real = X.loc[(X[column_name] <= column_mean) & (X['label'] == 1)]
-    below_fake = X.loc[(X[column_name] <= column_mean) & (X['label'] == 0)]
-
-    # find Entropy for labels
-    len_label = len(X.label)
-    count_distribution_of_labels = X.label.value_counts()
-
-    probabilities = [count / len_label for count in count_distribution_of_labels]
-    column_entropy = 0
-    for probability in probabilities:
-        column_entropy = column_entropy + probability * log2(probability)
-    print(-column_entropy)
